bf7c3037f313299a4c0f5fb0d552bcb7c7285be2
Wei-Han He <scsa32@gmail.com>
2019-05-18 17:34:13 +0800

    Move tools from my_av to tools

---
+++ tools/views.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from my_av.forms import PathForm
+from .utilities import nfos_finder, nfo_importer, videos_finder
+
+
+def list_nfos(request: WSGIRequest) -> HttpResponse:
+    source_path = request.GET['source-path']
+    path = Path(source_path)
+    paths = nfos_finder(path)
+    return render(request, 'tools/list-nfos.html', {'source_path': source_path, 'paths': paths})
+
+
+def tool(request):
+    return render(request, 'tools/tool.html', {'form': PathForm()})
+
+
+def parse_folder(request):
+    form = PathForm(request.GET)
+
+    if form.is_valid():
+        source_folder_path_str = form.cleaned_data['path']
+        source_folder_path = Path(source_folder_path_str)
+        nfos_path = nfos_finder(source_folder_path)
+        [nfo_importer(path) for path in nfos_path]
+
+    else:
+        nfos_path = []
+        form = PathForm()
+    return render(request, 'my_av/file.html', {'form': form,
+                                               'nfos_path': nfos_path})
+
+
+def list_movie(request):
+    form = PathForm(request.GET)
+    if form.is_valid():
+        path_str = form.cleaned_data['path']
+        path = Path(path_str)
+        movie_paths = videos_finder(path)
+        return render(request, 'tools/list-paths.html', {'paths': movie_paths})
+    else:
+        context = {'error': form.errors, 'form': PathForm()}
+        return render(request, 'tools/tool.html', context)
+
+
+def list_files(request, source_path: str):
+    if source_path:
+        path = Path(source_path)
+        movie_paths = videos_finder(path)
+        context = {'paths': movie_paths}
+    else:
+        context = dict()
+    return request(request, 'my_av/tools/list-files.html', context)
+
+
+def import_nfos(request: WSGIRequest) -> HttpResponse:
+    source_path = request.GET['source-path']
+    path = Path(source_path)
+    paths = nfos_finder(path)
+    for nfo in paths:
+        nfo_importer(nfo)
+
+    return redirect(to=reverse('admin:index'))
